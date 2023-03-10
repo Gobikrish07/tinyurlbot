9528e8dbe6a4104d01cbe943b99d0e72f6854087
Gobikrish07 <111223085+Gobikrish07@users.noreply.github.com>
2023-03-10 21:12:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import random
+from pyrogram import Client, filters
+import requests
+from config import *
+import asyncio
+
+# TinyURL API 
+
+API_KEY=API_KEY
+
+# Session Authorization
+
+app = Client("tinyurl_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+# Start Command
+
+@app.on_message(filters.command('start'))
+async def start_command_handler(client, message):
+    await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=(f"<b>𝙷𝚎𝚕𝚕𝚘 !! {message.from_user.mention} \n𝚂𝚎𝚗𝚍 𝚖𝚎 𝙻𝚘𝚗𝚐 𝚄𝚁𝙻, 𝙸'𝚕𝚕 𝚂𝚑𝚘𝚛𝚝𝚎𝚗 𝚒𝚝.</b>")
+    )
+
+# url shorten via the TinyURL API
+
+@app.on_message(filters.regex('https?://\S+'))
+async def url_message_handler(client, message):
+    try:
+        response = requests.get(f'https://tinyurl.com/api-create.php?url={message.text}&apikey={API_KEY}')
+        shortened_url = response.text
+        message.reply_text(f'𝙷𝚎𝚛𝚎 𝚒𝚜, \n𝚈𝚘𝚞𝚛 𝚂𝚑𝚘𝚛𝚝𝚎𝚗 𝙻𝚒𝚗𝚔 : {shortened_url}')
+    except:
+        message.reply_text('Sorry. Please Try Again.')
+
+# Check whether Bot Started or Idle !!
+
+print('Bot Started !!')
+
+# Run
+
+app.run()
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# # Enter your environment variables
+
+api_id = int(os.environ.get("API_ID", "25514026"))
+api_hash = os.environ.get("API_HASH", "e7fcf97f908583890b80814ef55292a8")
+bot_token = os.environ.get("BOT_TOKEN", "5938252449:AAEu5xzurXCyrT1zSNHcwrSIgiSPNNbjBVw")
+PICS = (os.environ.get('PICS', 'https://te.legra.ph/file/eba8951de13b3fafefc12.jpg')).split()
+API_KEY = os.environ.get("API_KEY", "RGwQls0GhQxeNwkgSUI1iAH7ly4znvIJjGblnQEPaixI9vEhBjrhQOaJQ3AT")
