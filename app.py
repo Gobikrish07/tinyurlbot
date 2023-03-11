4da50a170fca7b83a88bdf1d4f1fe7f89d077a2a
Gobikrish07 <111223085+Gobikrish07@users.noreply.github.com>
2023-03-11 16:41:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from flask import Flask
 import random
 from pyrogram import Client, filters
 import requests
@@ -39,4 +40,14 @@ print('Bot Started !!')
 
 # Run
 
-app.run()
+app = Flask(__name__)
+
+# ...
+
+if __name__ == '__main__':
+
+# Bind to PORT if defined, otherwise default to 5000.
+
+port = int(os.environ.get('PORT', 5000))
+
+app.run(host='0.0.0.0', port=port)
